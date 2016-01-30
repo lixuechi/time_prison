e46979fb789ca7c3d00d954951c0754fcc43157f
xuechi.li <lixuechi@hotmail.com>
2016-01-30 17:42:20 +0800

    + mouse control inherit Omni and clean code inside MouseControl

--- GGJ2016_temp/time_prison/Assets/Omni.cs
+++ GGJ2016_temp/time_prison/Assets/Omni.cs
@@ -3,6 +3,14 @@ using System.Collections;
 
 public class Omni : MonoBehaviour {
 
+	// Omni contains all of the global variables and serves as the parent class of all other classes
+
+	//<color>
+	public static int currSelectedColor = 0;
+	public const int COLOR_1 = 0;
+	public const int COLOR_2 = 1;
+	//</color>
+
 	void Start () {
 	
 	}
