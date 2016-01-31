7e6c177b547945565cdfb5012600d296c2a3d859
xuechi.li <lixuechi@hotmail.com>
2016-01-31 14:45:13 +0800

    + roof walls

---
+++ GGJ2016_temp/time_prison/Assets/TPsplashScreenControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TPsplashScreenControl : MonoBehaviour {
+
+	float oldTime = 0;
+	float currTime = 0;
+	AudioSource audioSource;
+
+	void Start () {
+		currTime = 0;
+
+		audioSource = this.GetComponent<AudioSource> ();
+	}
+
+	void Update () {
+	
+		currTime = Time.timeSinceLevelLoad;
+		Debug.Log ( (currTime - oldTime) + "");
+		if (currTime - oldTime >= 4) 
+		{
+			// load the next level
+			Application.LoadLevel("_ritual");
+		}
+	}
+}
