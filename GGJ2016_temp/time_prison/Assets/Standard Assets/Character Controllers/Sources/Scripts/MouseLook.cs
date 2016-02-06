0aa7692f9bcb7e9ed9d48abd09d35339646c5a0d
xuechi.li <lixuechi@hotmail.com>
2016-02-06 20:41:51 +0800

    * Windows/Mac/Linux/Web Player build 0.0.4
    - delete buggy 0.0.5
    * rebuild Windows/Mac/Linux/Web Player builds 0.0.4

--- GGJ2016_temp/time_prison/Assets/Standard Assets/Character Controllers/Sources/Scripts/MouseLook.cs
+++ GGJ2016_temp/time_prison/Assets/Standard Assets/Character Controllers/Sources/Scripts/MouseLook.cs
@@ -36,23 +36,20 @@ public class MouseLook : MonoBehaviour {
 
 	void Update ()
 	{
-		if (axes == RotationAxes.MouseXAndY)
-		{
-			float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+		if (axes == RotationAxes.MouseXAndY) {
+						float rotationX = transform.localEulerAngles.y + Input.GetAxis ("Mouse X") * sensitivityX;
 			
-			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+						rotationY += Input.GetAxis ("Mouse Y") * sensitivityY;
+						rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
 			
-			transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+						transform.localEulerAngles = new Vector3 (-rotationY, rotationX, 0);
 
-			_3_3_3.Rotate(new Vector3(-rotationY, rotationX, 0));
-		}
-		else if (axes == RotationAxes.MouseX)
-		{
-			transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+						//_3_3_3.Rotate(Input.GetAxis("Mouse Y") * sensitivityY, Input.GetAxis("Mouse X") * sensitivityX, 0);
+				} else if (axes == RotationAxes.MouseX) {
+						transform.Rotate (0, Input.GetAxis ("Mouse X") * sensitivityX, 0);
 
-			_3_3_3.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
-		}
+						//_3_3_3.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+				}
 		else
 		{
 			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
